16b69aecddef06eaca6bd17509c23d7c5926b660
liuzuolin1996 <liuzuolin1996@gmail.com>
2022-01-18 17:46:02 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message": "hello world"}
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="127.0.0.1", port=5000)
+
